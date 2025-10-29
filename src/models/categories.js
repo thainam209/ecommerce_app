894584a74eb43e66cf496006f55ce9c1a35d089b
thainam209 <thainam2092004@gmail.com>
2025-10-29 11:03:55 +0700

    creat category

---
+++ src/models/categories.js
@@ -0,0 +1,39 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Categories extends Model {
+    static associate(models) {
+      // Categories hasMany Products (một Categories có nhiều Products)
+      Categories.hasMany(models.Product, {
+        foreignKey: 'CategoryId',
+        as: 'products'
+      });
+    }
+  }
+  Categories.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+        allowNull:false,
+        type: DataTypes.STRING
+    },
+    description: DataTypes.TEXT,
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
+  }, {
+    sequelize,
+    modelName: 'Categories',
+    tableName: 'Categories'
+  });
+  return Categories;
+};
